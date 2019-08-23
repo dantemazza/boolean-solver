04f840127cb06d89d546718cfa22247f7948c930
dantemazza <47069410+dantemazza@users.noreply.github.com>
2019-08-23 11:15:43 -0400

    Add files via upload

--- quine-mccluskey/tabulation_main.py
+++ quine-mccluskey/tabulation_main.py
@@ -1,9 +1,24 @@
-test_case1 = open("test_case1.txt").read()
+function = open("test_case1.txt").readlines()
 
-print(test_case1)
+size = int(function[0])
+minTerms = []
+dontCare = len(function) > 2
+minTrueMaxFalse = function[1][0] == "m"
+#terms = function[1][2:(len(function[1])-2)].split(',')
+terms = [int(n) for n in function[1][2:(len(function[1])-2)].split(',')]
+if dontCare:
+    dcTerms = [int(n) for n in function[2][2:(len(function[2])-1)].split(',')]
 
-minTrueMaxFalse = test_case1[0] == "m"
+print(terms)
+print(dcTerms)
 
-print(minTrueMaxFalse)
+minTerms = []
 
-    
+if not minTrueMaxFalse:
+    for i in range(2**size):
+        if not ((i in terms) or (i in dcTerms)):
+            minTerms.append(i)
+else:
+    minTerms = terms
+
+print(minTerms)
